a7ecb5a23b9e84c16f7d96ea10e3af12b6316cc0
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-10-03 16:49:49 +0300

    Refactor App component for improved structure and maintainability

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
 // App.jsx
 import { Routes, Route } from 'react-router-dom';
-import Login from '../Login/Login';
+import LoginPage from '../Login/Login';
 import PrivacyPolicy from '../../pages/PrivacyPolicy/PrivacyPolicy';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
     </Routes>
   );
