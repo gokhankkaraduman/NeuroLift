9a42b1f11260cc05ad005e13bb6cc65fcf6368db
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-10-03 13:06:11 +0300

    Refactor main entry point for new app structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './css/global.css'
+import './css/variables.css'
+import App from './components/App/App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom'
+
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
