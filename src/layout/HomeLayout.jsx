e7eaf2c394a186b7677df7b58adfc55a7b9e5d96
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-10-28 22:24:54 +0300

    refactor(layout): adjust HomeLayout to new component structure

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,20 +1,31 @@
-import WelcomeText from '../components/WelcomeText/WelcomeText';
-import SummaryPanel from '../components/SummaryPanel/SummaryPanel';
+import WelcomeText from '../components/Home/WelcomeText/WelcomeText';
+import SummaryPanel from '../components/Home/SummaryPanel/SummaryPanel';
 import styles from './HomeLayout.module.css';
-import RecentEntries from '../components/RecentEntries/RecentEntries';
-import HomeSidebar from '../components/HomeSidebar/HomeSidebar';
+import RecentEntries from '../components/Home/RecentEntries/RecentEntries';
+import HomeSidebar from '../components/Home/HomeSidebar/HomeSidebar';
+import DailyMeditationSession from '../components/Home/DailyMeditationSession/DailyMeditationSession';
+import GoalEditor from '../components/Home/GoalEditor/GoalEditor';
 
 export default function HomeLayout() {
   return (
     <div className={styles.homeLayout}>
-      <div className={styles.rightDecoration}>
-        <WelcomeText />
-        <SummaryPanel />
-        <RecentEntries />
+      <div className={styles.welcomeTextContainer}>
+      <WelcomeText />
       </div>
-      <div className={styles.leftDecoration}>
-        <HomeSidebar />
+      <div className={styles.homeLayoutContainer}>
+        <div className={styles.rightDecoration}>
+          <SummaryPanel />
+          <RecentEntries />
+          <div className={styles.twoColumnSection}>
+            <DailyMeditationSession />
+            <GoalEditor />
+          </div>
+        </div>
+        <div className={styles.leftDecoration}>
+          <HomeSidebar />
+        </div>
       </div>
+
     </div>
   )
 }
