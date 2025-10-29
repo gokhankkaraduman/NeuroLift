297c8af8081ce7cf4dc0a416e4a6f9001a6b8cd4
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-10-29 04:26:50 +0300

    feat(layout): add HomeLayout and styles

---
+++ src/layout/HomeLayout/HomeLayout.jsx
@@ -0,0 +1,31 @@
+import WelcomeText from '../../components/Home/WelcomeText/WelcomeText';
+import SummaryPanel from '../../components/Home/SummaryPanel/SummaryPanel';
+import styles from './HomeLayout.module.css';
+import RecentEntries from '../../components/Home/RecentEntries/RecentEntries';
+import HomeSidebar from '../../components/Home/HomeSidebar/HomeSidebar';
+import DailyMeditationSession from '../../components/Home/DailyMeditationSession/DailyMeditationSession';
+import GoalEditor from '../../components/Home/GoalEditor/GoalEditor';
+
+export default function HomeLayout() {
+  return (
+    <div className={styles.homeLayout}>
+      <div className={styles.welcomeTextContainer}>
+      <WelcomeText />
+      </div>
+      <div className={styles.homeLayoutContainer}>
+        <div className={styles.rightDecoration}>
+          <SummaryPanel />
+          <RecentEntries />
+          <div className={styles.twoColumnSection}>
+            <DailyMeditationSession />
+            <GoalEditor />
+          </div>
+        </div>
+        <div className={styles.leftDecoration}>
+          <HomeSidebar />
+        </div>
+      </div>
+
+    </div>
+  )
+}
